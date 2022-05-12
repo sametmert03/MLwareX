5c3c0a3d250d8bf2c5222a8866f1f3f23bb7b10d
sametmert03 <57039677+sametmert03@users.noreply.github.com>
2022-05-12 21:56:28 +0300

    init

---
+++ MalConv-keras-master/MalConv-keras-master/src/backup.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+hour_and_minute = datetime.datetime.now().strftime("%c")
+print(hour_and_minute)
+
+
+
+
+
+
---
+++ MalConv-keras-master/MalConv-keras-master/src/ex2.py
@@ -0,0 +1,34 @@
+import random
+
+import matplotlib.pyplot as plt
+import csv
+import random
+
+import pandas as pd
+
+x = []
+y = []
+Names = []
+Values = []
+i = 0
+df = pd.read_csv('E:\\Studies\\TEZ\\MalConv-keras-master\\MalConv-keras-master\\src\\samp\\result.csv')
+shuffled_df = df.sample(frac=1)
+aa = df.iloc[981:,2:3]
+
+aa2 = shuffled_df.iloc[:,1:2]
+with open('E:\\Studies\\TEZ\\MalConv-keras-master\\MalConv-keras-master\\src\\samp\\result.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ',')
+    for row in plots:
+        i+=1
+        x.append(row[1])
+        y.append((row[2]))
+        if i == 500:
+            break
+
+
+
+plt.hist(aa)
+plt.show()
+
+plt.hist(aa2)
+plt.show()
